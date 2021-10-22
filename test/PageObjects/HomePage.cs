b3cd53ef2b28c58bc318f2ed8ca278169c3e362d
g-aleprojetos <g.aleprojetos@gmail.com>
2021-10-21 21:16:20 -0300

    Atualização do DadoChromeAbertoIrHomePage

--- test/PageObjects/HomePage.cs
+++ test/PageObjects/HomePage.cs
@@ -1,33 +1,24 @@
 ﻿using OpenQA.Selenium;
-using test.Fixtures;
-using Xunit;
+using test.Dados;
 
 namespace test.PageObjects
 {
     public class HomePage
     {
-        [Collection("Chrome Driver")]
-        public class AoNavegarParaHome
+        IWebDriver driver;
+        private DadosGerais dados;
+
+        public HomePage()
         {
-            private IWebDriver driver;
-
-            //Setup
-            public AoNavegarParaHome(TestFixture fixture)
-            {
-                driver = fixture.Driver;
-            }
-
-            [Fact]
-            public void DadoChromeAbertoDeveMostrarLeiloesNoTitulo()
-            {
-                //arrange
-
-                //act
-                driver.Navigate().GoToUrl("http://localhost:5000");
-
-                //assert
-                Assert.Contains("Leilões", driver.Title);
-            }
+            dados = new DadosGerais();
         }
+
+        public void OpenPage()
+        {
+
+            driver.Navigate().GoToUrl(dados.URL);
+
+        }
+
     }
 }
